fd9be7a763f2e70b5ee493d014b4b01a86d26df6
MichaelT115 <mtc5284@rit.edu>
2019-05-31 13:34:14 -0400

    Change agent.

--- UnitySDK/Assets/ML-DodgeBall/Scripts/AgentDodgeBallPlayer.cs
+++ UnitySDK/Assets/ML-DodgeBall/Scripts/AgentDodgeBallPlayer.cs
@@ -1,134 +1,156 @@
-﻿using UnityEngine;
+﻿//Put this script on your blue cube.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using MLAgents;
 
-public class AgentDodgeBallPlayer : Agent, IObservable
+public class AgentDodgeBallPlayer : Agent
 {
-    public float moveSpeed;
-    public GameManager gameManager;
-    public AgentDodgeBallPlayer rival;
+    public float spawnAreaMarginMultiplier = 3;
+    public float agentRunSpeed = 5;
+    public float rayDistance = 12f;
+    public float[] rayAngles = { 0f, 15, 30, 45f, 60, 75, 90f, 105, 120, 135f, 150, 165, 180f };
+    public string[] detectableObjects = new[] { "goal", "wall" };
 
-    [HideInInspector]
-    public Rigidbody agentRb;
+    /// <summary>
+    /// The ground. The bounds are used to spawn the elements.
+    /// </summary>
+	public GameObject ground;
 
-    public Vector3 StartPosition;
-    public Quaternion StartRotation;
+    /// <summary>
+    /// The area bounds.
+    /// </summary>
+	[HideInInspector]
+    public Bounds areaBounds;
 
-    public bool hasBall;
-    public Transform ballHolder;
+    public bool useVectorObs;
+    
+    Rigidbody agentRB;  //cached on initialization
+    RayPerception rayPer;
 
-    [Header("Debug")]
-    public float angle, distanceToBall;
 
     public override void InitializeAgent()
     {
         base.InitializeAgent();
-        agentRb = GetComponent<Rigidbody>();
+        rayPer = GetComponent<RayPerception>();
 
-        StartPosition = transform.position;
-        StartRotation = transform.rotation;
+        // Cache the agent rigidbody
+        agentRB = GetComponent<Rigidbody>();
+
+        // Get the ground's bounds
+        areaBounds = ground.GetComponent<Collider>().bounds;
     }
 
     public override void CollectObservations()
     {
-        Vector3 dodgeBallPosition = gameManager.dodgeBall.transform.position;
-        Vector3 toDodgeBall = gameManager.dodgeBall.transform.position - transform.position;
-
-        angle = Vector3.SignedAngle(transform.forward, toDodgeBall, Vector3.up);
-        prevDistance = distanceToBall;
-        distanceToBall = toDodgeBall.sqrMagnitude;
-
-        AddVectorObs(angle);
-        AddVectorObs(distanceToBall);
-
-        Debug.DrawRay(transform.position, transform.forward * 2, Color.white, 0.5f);
-        Debug.DrawRay(transform.position, toDodgeBall, Color.red, 0.5f);
+        if (useVectorObs)
+        {
+            AddVectorObs(rayPer.Perceive(rayDistance, rayAngles, detectableObjects, 0f, 0f));
+        }
     }
 
-    #region Actions
-    public override void AgentAction(float[] vectorAction, string textAction)
+    /// <summary>
+    /// Use the ground's bounds to pick a random spawn position.
+    /// </summary>
+    public Vector3 GetRandomSpawnPos()
     {
-        MoveAgent(vectorAction);
+        bool foundNewSpawnLocation = false;
+        Vector3 randomSpawnPos = Vector3.zero;
+        while (foundNewSpawnLocation == false)
+        {
+            float randomPosX = Random.Range(-areaBounds.extents.x * spawnAreaMarginMultiplier,
+                                areaBounds.extents.x * spawnAreaMarginMultiplier);
+
+            float randomPosZ = Random.Range(-areaBounds.extents.z * spawnAreaMarginMultiplier,
+                                            areaBounds.extents.z * spawnAreaMarginMultiplier);
+            randomSpawnPos = ground.transform.position + new Vector3(randomPosX, 1f, randomPosZ);
+            if (Physics.CheckBox(randomSpawnPos, new Vector3(2.5f, 0.01f, 2.5f)) == false)
+            {
+                foundNewSpawnLocation = true;
+            }
+        }
+        return randomSpawnPos;
     }
-    float prevDistance = float.PositiveInfinity;
 
-    public void MoveAgent(float[] act)
+    /// <summary>
+    /// Moves the agent according to the selected action.
+    /// </summary>
+	public void MoveAgent(float[] act)
     {
-        Vector3 move = Vector3.zero;
+        Vector3 dirToGo = Vector3.zero;
+        Vector3 rotateDir = Vector3.zero;
 
-        int movementAction = Mathf.FloorToInt(act[0]);
+        int action = Mathf.FloorToInt(act[0]);
 
-        bool throwBall = Mathf.FloorToInt(act[1]) == 1;
-
-        switch (movementAction)
+        // Goalies and Strikers have slightly different action spaces.
+        switch (action)
         {
             case 1:
-                move = transform.forward;
+                dirToGo = transform.forward * 1f;
                 break;
             case 2:
-                move = -transform.forward;
+                dirToGo = transform.forward * -1f;
                 break;
             case 3:
-                move = transform.right;
+                rotateDir = transform.up * 1f;
                 break;
             case 4:
-                move = -transform.right;
+                rotateDir = transform.up * -1f;
+                break;
+            case 5:
+                dirToGo = transform.right * -0.25f;
+                break;
+            case 6:
+                dirToGo = transform.right * 0.25f;
                 break;
         }
+        transform.Rotate(rotateDir, Time.fixedDeltaTime * 200f);
+        agentRB.AddForce(dirToGo * agentRunSpeed,
+                         ForceMode.VelocityChange);
 
-        if (throwBall && gameManager.dodgeBall.isHeld)
+    }
+
+    /// <summary>
+    /// Called every step of the engine. Here the agent takes an action.
+    /// </summary>
+	public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        // Move the agent using the action.
+        MoveAgent(vectorAction);
+
+        // Penalty given each step to encourage agent to finish task quickly.
+        AddReward(-1f / agentParameters.maxStep);
+    }
+
+    /// <summary>
+    /// In the editor, if "Reset On Done" is checked then AgentReset() will be 
+    /// called automatically anytime we mark done = true in an agent script.
+    /// </summary>
+	public override void AgentReset()
+    {
+        int rotation = Random.Range(0, 4);
+        float rotationAngle = rotation * 90f;
+
+        transform.position = GetRandomSpawnPos();
+        agentRB.velocity = Vector3.zero;
+        agentRB.angularVelocity = Vector3.zero;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<DodgeBall>())
         {
-            ThrowBall();
+            GrabBall(collision.gameObject.GetComponent<DodgeBall>());
         }
-
-        agentRb.MovePosition(transform.position + move * moveSpeed * Time.deltaTime);
     }
 
-    [ContextMenu("Throw Ball")]
-    public void ThrowBall()
+    private void GrabBall(DodgeBall dodgeBall)
     {
-        gameManager.dodgeBall.rb.isKinematic = false;
+        AddReward(5f);
 
-        gameManager.dodgeBall.transform.parent = gameManager.transform;
-        gameManager.dodgeBall.rb.AddForce(transform.forward * 25, ForceMode.Impulse);
-        gameManager.dodgeBall.isHeld = false;
-    }
-    #endregion
+        dodgeBall.transform.position = GetRandomSpawnPos();
 
-    void OnCollisionEnter(Collision c)
-    {
-        if (c.gameObject.GetComponent<DodgeBall>() && !gameManager.dodgeBall.isHeld)
-        {
-            Debug.Log("Collided with ball");
-
-            AddReward(1);
-
-            Done();
-            return;
-
-            gameManager.dodgeBall.transform.SetParent(ballHolder);
-            gameManager.dodgeBall.transform.localPosition = Vector3.zero;
-
-            gameManager.dodgeBall.isHeld = true;
-            gameManager.dodgeBall.rb.isKinematic = true;
-        }
-    }
-
-    public float[] GetObservations()
-    {
-        return new float[]
-        {
-            transform.position.x,
-            transform.position.z,
-        };
-    }
-
-    public override void AgentReset()
-    {
-        transform.SetPositionAndRotation(StartPosition, StartRotation);
-        hasBall = false;
-
-        gameManager.dodgeBall.transform.localPosition = new Vector3(0, 0.75f, 0);
-        gameManager.dodgeBall.rb.velocity = Vector3.zero;
+        Done();
     }
 }
-
